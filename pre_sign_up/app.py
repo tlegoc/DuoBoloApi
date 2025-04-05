45edace5c9a7cf7eabdba0db5390894fe5afe26c
Théo Le Goc <theo@legoc.net>
2025-04-05 02:18:40 -0400

    renamed and update cognito lambda event functions

--- pre_sign_up/app.py
+++ pre_sign_up/app.py
@@ -4,4 +4,15 @@ def lambda_handler(event, context):
     if not re.match(r'^[A-Za-z_\-0-9.]+$', event['userName']):
         raise Exception("Invalid username")
 
+    # Confirm the user
+    event['response']['autoConfirmUser'] = True
+
+    # Set the email as verified if it is in the request
+    if 'email' in event['request']['userAttributes']:
+        event['response']['autoVerifyEmail'] = True
+
+    # Set the phone number as verified if it is in the request
+    if 'phone_number' in event['request']['userAttributes']:
+        event['response']['autoVerifyPhone'] = True
+
     return event
