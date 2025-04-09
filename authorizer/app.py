8894fe296b955fcf0ff7e341ef8301bb6e086ef2
Théo Le Goc <theo@legoc.net>
2025-04-09 02:03:10 -0400

    updated dependencies, working lambda authorizer but no api gateway connection

--- authorizer/app.py
+++ authorizer/app.py
@@ -1,229 +1,91 @@
-import os
-from jwt.algorithms import RSAAlgorithm
-
-import re
 import jwt
-import json
-import sys
-import urllib
+from jwt import ExpiredSignatureError, DecodeError
+from jwt import PyJWKClient
+import requests
+import os
 
-region = os.environ.get("COGNITO_REGION")
-userpoolId = os.environ.get("COGNITO_USER_POOL_ID")
-appClientId = os.environ.get("COGNITO_USER_POOL_CLIENT_ID")
-keysUrl = 'https://cognito-idp.{}.amazonaws.com/{}/.well-known/jwks.json'.format(region, userpoolId)
+# Set up your Cognito pool data
+COGNITO_USER_POOL_ID = os.environ.get("COGNITO_USER_POOL_ID")
+COGNITO_REGION = os.environ.get("COGNITO_REGION")
+COGNITO_APP_CLIENT_ID = os.environ.get("COGNITO_USER_POOL_CLIENT_ID")
+COGNITO_POOL_ISSUER = f"https://cognito-idp.{COGNITO_REGION}.amazonaws.com/{COGNITO_USER_POOL_ID}"
+
+
+# Fetch Cognito Pool public keys dynamically (public keys used to validate JWT)
+def get_cognito_public_keys():
+    url = f"https://cognito-idp.{COGNITO_REGION}.amazonaws.com/{COGNITO_USER_POOL_ID}/.well-known/jwks.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()['keys']
+    else:
+        raise Exception("Unable to fetch Cognito public keys")
+
+
+# Validate JWT token
+def validate_jwt(token):
+    unverified_header = jwt.get_unverified_header(token)
+    if unverified_header is None or 'kid' not in unverified_header:
+        raise Exception("Authorization malformed")
+    #
+    # # Fetch public keys
+    # public_keys = get_cognito_public_keys()
+    #
+    # rsa_key = {}
+    # for key in public_keys:
+    #     if key['kid'] == unverified_header['kid']:
+    #         rsa_key = {
+    #             'kty': key['kty'],
+    #             'kid': key['kid'],
+    #             'use': key['use'],
+    #             'n': key['n'],
+    #             'e': key['e']
+    #         }
+
+    url = f"https://cognito-idp.{COGNITO_REGION}.amazonaws.com/{COGNITO_USER_POOL_ID}/.well-known/jwks.json"
+    jwks_client = PyJWKClient(url, headers=unverified_header)
+    rsa_key = jwks_client.get_signing_key_from_jwt(token)
+
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=['RS256'],
+                audience=COGNITO_APP_CLIENT_ID,
+                issuer=COGNITO_POOL_ISSUER
+            )
+            return payload
+        except ExpiredSignatureError:
+            raise Exception("Token is expired")
+        except DecodeError:
+            raise Exception("Unable to decode token")
+    else:
+        raise Exception("Unable to find appropriate key")
+
 
 def lambda_handler(event, context):
-    bearerToken = event['authorizationToken']
-    methodArn = event['methodArn']
-    print("Client token: " + bearerToken)
-    print("Method ARN: " + methodArn)
+    # Extract the token from the Authorization header
+    token = event['querystring'].get('token', None)
 
-    response = urllib.urlopen(keysUrl)
-    keys = json.loads(response.read())['keys']
+    if not token:
+        raise Exception("Unauthorized")
 
-    jwtToken = bearerToken.split(' ')[-1]
-    header = jwt.get_unverified_header(jwtToken)
-    kid = header['kid']
-
-    jwkValue = findJwkValue(keys, kid)
-    publicKey = RSAAlgorithm.from_jwk(json.dumps(jwkValue))
-
-    decoded = decodeJwtToken(jwtToken, publicKey)
-    print('Decoded token: ' + json.dumps(decoded))
-    principalId = decoded['cognito:username']
-
-    methodArn = event['methodArn'].split(':')
-    apiGatewayArnTmp = methodArn[5].split('/')
-    awsAccountId = methodArn[4]
-
-    policy = AuthPolicy(principalId, awsAccountId)
-    policy.restApiId = apiGatewayArnTmp[0]
-    policy.region = methodArn[3]
-    policy.stage = apiGatewayArnTmp[1]
-    #policy.denyAllMethods()
-    policy.allowAllMethods()
-
-    # Finally, build the policy
-    authResponse = policy.build()
-
-    # new! -- add additional key-value pairs associated with the authenticated principal
-    # these are made available by APIGW like so: $context.authorizer.<key>
-    # additional context is cached
-    context = {
-        'key': 'value',  # $context.authorizer.key -> value
-        'number': 1,
-        'bool': True
-    }
-    # context['arr'] = ['foo'] <- this is invalid, APIGW will not accept it
-    # context['obj'] = {'foo':'bar'} <- also invalid
-
-    authResponse['context'] = context
-
-    return authResponse
-
-def findJwkValue(keys, kid):
-    for key in keys:
-        if key['kid'] == kid:
-            return key
-
-def decodeJwtToken(token, publicKey):
     try:
-        decoded=jwt.decode(token, publicKey, algorithms=['RS256'], audience=appClientId)
-        return decoded
-    except Exception as e:
-        print(e)
-        raise
+        # Validate the JWT token using the Cognito public key
+        payload = validate_jwt(token)
 
-class HttpVerb:
-    GET = 'GET'
-    POST = 'POST'
-    PUT = 'PUT'
-    PATCH = 'PATCH'
-    HEAD = 'HEAD'
-    DELETE = 'DELETE'
-    OPTIONS = 'OPTIONS'
-    ALL = '*'
-
-
-class AuthPolicy(object):
-    # The AWS account id the policy will be generated for. This is used to create the method ARNs.
-    awsAccountId = ''
-    # The principal used for the policy, this should be a unique identifier for the end user.
-    principalId = ''
-    # The policy version used for the evaluation. This should always be '2012-10-17'
-    version = '2012-10-17'
-    # The regular expression used to validate resource paths for the policy
-    pathRegex = '^[/.a-zA-Z0-9-\*]+$'
-
-    '''Internal lists of allowed and denied methods.
-
-    These are lists of objects and each object has 2 properties: A resource
-    ARN and a nullable conditions statement. The build method processes these
-    lists and generates the approriate statements for the final policy.
-    '''
-    allowMethods = []
-    denyMethods = []
-
-    # The API Gateway API id. By default this is set to '*'
-    restApiId = '*'
-    # The region where the API is deployed. By default this is set to '*'
-    region = '*'
-    # The name of the stage used in the policy. By default this is set to '*'
-    stage = '*'
-
-    def __init__(self, principal, awsAccountId):
-        self.awsAccountId = awsAccountId
-        self.principalId = principal
-        self.allowMethods = []
-        self.denyMethods = []
-
-    def _addMethod(self, effect, verb, resource, conditions):
-        '''Adds a method to the internal lists of allowed or denied methods. Each object in
-        the internal list contains a resource ARN and a condition statement. The condition
-        statement can be null.'''
-        if verb != '*' and not hasattr(HttpVerb, verb):
-            raise NameError('Invalid HTTP verb ' + verb + '. Allowed verbs in HttpVerb class')
-        resourcePattern = re.compile(self.pathRegex)
-        if not resourcePattern.match(resource):
-            raise NameError('Invalid resource path: ' + resource + '. Path should match ' + self.pathRegex)
-
-        if resource[:1] == '/':
-            resource = resource[1:]
-
-        resourceArn = 'arn:aws:execute-api:{}:{}:{}/{}/{}/{}'.format(self.region, self.awsAccountId, self.restApiId, self.stage, verb, resource)
-
-        if effect.lower() == 'allow':
-            self.allowMethods.append({
-                'resourceArn': resourceArn,
-                'conditions': conditions
-            })
-        elif effect.lower() == 'deny':
-            self.denyMethods.append({
-                'resourceArn': resourceArn,
-                'conditions': conditions
-            })
-
-    def _getEmptyStatement(self, effect):
-        '''Returns an empty statement object prepopulated with the correct action and the
-        desired effect.'''
-        statement = {
-            'Action': 'execute-api:Invoke',
-            'Effect': effect[:1].upper() + effect[1:].lower(),
-            'Resource': []
-        }
-
-        return statement
-
-    def _getStatementForEffect(self, effect, methods):
-        '''This function loops over an array of objects containing a resourceArn and
-        conditions statement and generates the array of statements for the policy.'''
-        statements = []
-
-        if len(methods) > 0:
-            statement = self._getEmptyStatement(effect)
-
-            for curMethod in methods:
-                if curMethod['conditions'] is None or len(curMethod['conditions']) == 0:
-                    statement['Resource'].append(curMethod['resourceArn'])
-                else:
-                    conditionalStatement = self._getEmptyStatement(effect)
-                    conditionalStatement['Resource'].append(curMethod['resourceArn'])
-                    conditionalStatement['Condition'] = curMethod['conditions']
-                    statements.append(conditionalStatement)
-
-            if statement['Resource']:
-                statements.append(statement)
-
-        return statements
-
-    def allowAllMethods(self):
-        '''Adds a '*' allow to the policy to authorize access to all methods of an API'''
-        self._addMethod('Allow', HttpVerb.ALL, '*', [])
-
-    def denyAllMethods(self):
-        '''Adds a '*' allow to the policy to deny access to all methods of an API'''
-        self._addMethod('Deny', HttpVerb.ALL, '*', [])
-
-    def allowMethod(self, verb, resource):
-        '''Adds an API Gateway method (Http verb + Resource path) to the list of allowed
-        methods for the policy'''
-        self._addMethod('Allow', verb, resource, [])
-
-    def denyMethod(self, verb, resource):
-        '''Adds an API Gateway method (Http verb + Resource path) to the list of denied
-        methods for the policy'''
-        self._addMethod('Deny', verb, resource, [])
-
-    def allowMethodWithConditions(self, verb, resource, conditions):
-        '''Adds an API Gateway method (Http verb + Resource path) to the list of allowed
-        methods and includes a condition for the policy statement. More on AWS policy
-        conditions here: http://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements.html#Condition'''
-        self._addMethod('Allow', verb, resource, conditions)
-
-    def denyMethodWithConditions(self, verb, resource, conditions):
-        '''Adds an API Gateway method (Http verb + Resource path) to the list of denied
-        methods and includes a condition for the policy statement. More on AWS policy
-        conditions here: http://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements.html#Condition'''
-        self._addMethod('Deny', verb, resource, conditions)
-
-    def build(self):
-        '''Generates the policy document based on the internal lists of allowed and denied
-        conditions. This will generate a policy with two main statements for the effect:
-        one statement for Allow and one statement for Deny.
-        Methods that includes conditions will have their own statement in the policy.'''
-        if ((self.allowMethods is None or len(self.allowMethods) == 0) and
-                (self.denyMethods is None or len(self.denyMethods) == 0)):
-            raise NameError('No statements defined for the policy')
-
-        policy = {
-            'principalId': self.principalId,
-            'policyDocument': {
-                'Version': self.version,
-                'Statement': []
+        # If we reach here, the token is valid
+        return {
+            "principalId": payload["sub"],  # This can be any user-specific identifier
+            "policyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [{
+                    "Action": "execute-api:Invoke",
+                    "Effect": "Allow",
+                    "Resource": event["routeArn"]
+                }]
             }
         }
-
-        policy['policyDocument']['Statement'].extend(self._getStatementForEffect('Allow', self.allowMethods))
-        policy['policyDocument']['Statement'].extend(self._getStatementForEffect('Deny', self.denyMethods))
-
-        return policy
+    except Exception as e:
+        print(e)
+        raise Exception("Unauthorized")
